5d3c91fd31dbe67c465fbd851899adf4af300234
Sebastian <sebastian.mazur.p@gmail.com>
2023-08-08 18:29:07 +0200

    Create flashcards list

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,45 @@ import { Card } from "./components/Card";
 
 import "./App.css";
 import React, { useState } from "react";
+import { NewCard } from "./components/NewCard";
+
+interface CardI {
+  id: string;
+  question: string;
+  answer: string;
+}
 
 function App() {
   const [editMode, setEditMode] = useState<boolean>(false);
+  const [cards, setCards] = useState<CardI[]>([
+    {
+      id: "1",
+      question: "diffrence beetwen Observable and Promise",
+      answer:
+        "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+    },
+    {
+      id: "2",
+      question: "diffrence beetwen Observable and Promise",
+      answer: "Observable is better",
+    },
+    {
+      id: "3",
+      question: "diffrence beetwen Observable and Promise",
+      answer: "aaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    },
+    {
+      id: "4",
+      question: "diffrence beetwen Observable and Promise",
+      answer:
+        "Observable is better,lorffffffffssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+    },
+    {
+      id: "5",
+      question: "diffrence beetwen Observable and Promise",
+      answer: "elo",
+    },
+  ]);
   const changeMode = () => {
     setEditMode(!editMode);
   };
@@ -14,11 +50,20 @@ function App() {
     <AppLayout>
       <AppHeader fishkappiesLength={5} changeMode={changeMode} />
       <div className="cards_container">
-        {" "}
         {editMode ? (
-          <Card editMode={editMode} setEditMode={setEditMode} />
-        ) : (
-          <></>
+          <NewCard editMode={editMode} setEditMode={setEditMode} />
+        ) : null}
+        {cards.length > 0 && (
+          <>
+            {cards.map(card => (
+              <Card
+                key={card.id}
+                id={card.id}
+                question={card.question}
+                answer={card.answer}
+              />
+            ))}
+          </>
         )}
       </div>
     </AppLayout>
