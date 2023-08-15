a283fa42b0d03f56c1a378fd39736fc070689ce8
Sebastian <sebastian.mazur.p@gmail.com>
2023-08-15 14:03:43 +0200

    Test adding flashcard

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,60 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { addFishCard } from "../services/ApiService"; // Podaj właściwą ścieżkę do ApiService
+
+const server = setupServer(
+  rest.post(
+    "https://training.nerdbord.io/api/v1/fischkapp/flashcards",
+    (req, res, ctx) => {
+      // Tutaj możesz sprawdzić, czy przekazane dane są zgodne z oczekiwaniami
+      const { front, back } = req.body as { front: string; back: string };
+      if (front && back) {
+        // Możesz zwrócić dowolną odpowiedź na to żądanie POST
+        return res(
+          ctx.status(201),
+          ctx.json({ message: "Card added successfully" })
+        );
+      } else {
+        return res(
+          ctx.status(400),
+          ctx.json({ error: "Front and back are required" })
+        );
+      }
+    }
+  )
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("addFishCard", () => {
+  it("adds a fish card to the server", async () => {
+    const front = "Test Front";
+    const back = "Test Back";
+
+    const response = await addFishCard(front, back);
+
+    await expect(response).toEqual({ message: "Card added successfully" });
+  });
+
+  it("handles missing front and back", async () => {
+    const front = "";
+    const back = "";
+
+    await expect(addFishCard(front, back)).rejects.toThrow(
+      "Front and back are required"
+    );
+  });
+
+  it("handles missing front", async () => {
+    const front = "";
+    const back = "back";
+    await expect(addFishCard(front, back)).rejects.toThrow("Front is required");
+  });
+  it("handles missing front", async () => {
+    const front = "front";
+    const back = "";
+    await expect(addFishCard(front, back)).rejects.toThrow("Back is required");
+  });
+});
